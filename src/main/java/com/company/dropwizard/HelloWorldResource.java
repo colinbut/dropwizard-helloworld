66d92822abbf00ef8b4c4b808af8ddd16d94f932
Colin But <colinbut@users.noreply.github.com>
2016-08-20 19:47:28 +0100

    added resource

---
+++ src/main/java/com/company/dropwizard/HelloWorldResource.java
@@ -0,0 +1,49 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.company.dropwizard;
+
+import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Optional;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * The resource
+ *
+ * DropWizard's meat & potatoes :)
+ *
+ * Ideally be stateless & immutable as it will be accessed by multiple threads
+ *
+ * @author colin
+ */
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorldResource {
+
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloWorldResource(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        final String value = String.format(template, name.or(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+
+}
