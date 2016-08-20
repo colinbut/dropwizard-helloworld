64d046f737b69b938fea839a24c56c8bab8e083c
Colin But <colinbut@users.noreply.github.com>
2016-08-20 19:19:24 +0100

    wrote up configuration class

--- src/main/java/com/company/dropwizard/HelloWorldConfiguration.java
+++ src/main/java/com/company/dropwizard/HelloWorldConfiguration.java
@@ -2,9 +2,52 @@ package com.company.dropwizard;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.NotEmpty;
 
+/**
+ * A configuration class blueprint for a configuration object in DropWizard
+ *
+ * Jackson does the mapping from YAML to Configuration object
+ *
+ * @author colin
+ */
 public class HelloWorldConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    // Annotated with Hibernate's @NotEmpty to allow throw exception upon startup
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    //region getters and setters
+
+    /*
+     * Annotated with @JsonProperty to allow (de)serialization between properties
+     * from/to a YAML configuration file
+     */
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    //endregion
+
 }
