9f78f5af7c8f74a274f7cd72d5b797e307d8e9be
Colin But <colinbut@users.noreply.github.com>
2016-08-20 19:53:29 +0100

    added a HealthCheck feature

---
+++ src/main/java/com/company/dropwizard/TemplateHealthCheck.java
@@ -0,0 +1,32 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.company.dropwizard;
+
+import com.codahale.metrics.health.HealthCheck;
+
+/**
+ * A healthcheck class that does what its namesake
+ *
+ * @author colin
+ */
+public class TemplateHealthCheck extends HealthCheck {
+
+    private final String template;
+
+    public TemplateHealthCheck(String template) {
+        this.template = template;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        final String saying = String.format(template, "TEST");
+        if (!saying.contains("TEST")) {
+            return Result.unhealthy("template doesn't include a name");
+        }
+        return Result.healthy();
+    }
+    
+}
